d4325c63336896d7d730e81b7a745905534e972e
sammyne <sammyne@users.noreply.github.com>
2022-12-15 08:20:15 +0000

    Deploying to gh-pages from @ sammyne/hash-rs@904863c072119a51d441c48f8d2b34a6c771ace3 🚀

--- source-files.js
+++ source-files.js
@@ -1,4 +1,4 @@
 var sourcesIndex = JSON.parse('{\
-"hash":["",[["crc32",[],["digest.rs","mod.rs","simple.rs","slicing8.rs"]]],["lib.rs"]]\
+"hash":["",[["crc32",[],["digest.rs","mod.rs","simple.rs","slicing8.rs"]],["crc64",[],["digest.rs","mod.rs","slicing8.rs"]]],["lib.rs"]]\
 }');
 createSourceSidebar();
