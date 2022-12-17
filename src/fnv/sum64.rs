681ec3ac8640eab330347217d2257c715e02535c
sammyne <xiangminli@alumni.sjtu.edu.cn>
2022-12-17 07:11:23 +0000

    feature(fnv): impl new64

---
+++ src/fnv/sum64.rs
@@ -0,0 +1,60 @@
+use std::io::Write;
+
+use crate::fnv::{OFFSET64, PRIME64};
+use crate::{Hash, Hash64};
+
+pub struct Sum64(u64);
+
+impl Sum64 {
+    pub fn new() -> Self {
+        Self(OFFSET64)
+    }
+}
+
+impl Hash for Sum64 {
+    fn sum(&mut self, b: Option<Vec<u8>>) -> Vec<u8> {
+        let s = self.0.to_be_bytes();
+        match b {
+            Some(mut v) => {
+                v.extend_from_slice(&s);
+                v
+            }
+            None => s.to_vec(),
+        }
+    }
+
+    fn reset(&mut self) {
+        self.0 = OFFSET64;
+    }
+
+    fn size(&self) -> isize {
+        8
+    }
+
+    fn block_size(&self) -> isize {
+        1
+    }
+}
+
+impl Hash64 for Sum64 {
+    fn sum64(&mut self) -> u64 {
+        self.0
+    }
+}
+
+impl Write for Sum64 {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        let mut h = self.0;
+        for &v in buf {
+            h = h.wrapping_mul(PRIME64);
+            h ^= v as u64;
+        }
+        self.0 = h;
+
+        Ok(buf.len())
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        Ok(())
+    }
+}
