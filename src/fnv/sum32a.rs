211c2de3e4a471d9c78f96943c9bb401f97d50da
sammyne <xiangminli@alumni.sjtu.edu.cn>
2022-12-17 07:04:23 +0000

    feature(fnv): impl new32a

---
+++ src/fnv/sum32a.rs
@@ -0,0 +1,60 @@
+use std::io::Write;
+
+use crate::fnv::{OFFSET32, PRIME32};
+use crate::{Hash, Hash32};
+
+pub struct Sum32a(u32);
+
+impl Sum32a {
+    pub fn new() -> Self {
+        Self(OFFSET32)
+    }
+}
+
+impl Hash for Sum32a {
+    fn sum(&mut self, b: Option<Vec<u8>>) -> Vec<u8> {
+        let s = self.0.to_be_bytes();
+        match b {
+            Some(mut v) => {
+                v.extend_from_slice(&s);
+                v
+            }
+            None => s.to_vec(),
+        }
+    }
+
+    fn reset(&mut self) {
+        self.0 = OFFSET32;
+    }
+
+    fn size(&self) -> isize {
+        4
+    }
+
+    fn block_size(&self) -> isize {
+        1
+    }
+}
+
+impl Hash32 for Sum32a {
+    fn sum32(&mut self) -> u32 {
+        self.0
+    }
+}
+
+impl Write for Sum32a {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        let mut h = self.0;
+        for &v in buf {
+            h ^= v as u32;
+            h = h.wrapping_mul(PRIME32);
+        }
+        self.0 = h;
+
+        Ok(buf.len())
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        Ok(())
+    }
+}
--- src/fnv/tests/mod.rs
+++ src/fnv/tests/mod.rs
@@ -6,8 +6,18 @@ fn golden32() {
 }
 
 #[test]
+fn golden32a() {
+    test_golden(super::new32a(), GOLDEN32A_TEST_VECTOR.as_slice());
+}
+
+#[test]
 fn integrity32() {
     test_integrity32(super::new32());
+}
+
+#[test]
+fn integrity32a() {
+    test_integrity32(super::new32a());
 }
 
 fn test_golden<H>(h: H, test_vector: &[Test])
@@ -91,5 +101,12 @@ lazy_static::lazy_static! {
     Test::new(&[0x05, 0x0c, 0x5d, 0x7e], "a"),
     Test::new(&[0x70, 0x77, 0x2d, 0x38], "ab"),
     Test::new(&[0x43, 0x9c, 0x2f, 0x4b], "abc"),
+  ];
+
+  static ref GOLDEN32A_TEST_VECTOR: Vec<Test>=vec![
+    Test::new(&[0x81, 0x1c, 0x9d, 0xc5], ""),
+    Test::new(&[0xe4, 0x0c, 0x29, 0x2c], "a"),
+    Test::new(&[0x4d, 0x25, 0x05, 0xca], "ab"),
+    Test::new(&[0x1a, 0x47, 0xe9, 0x0b], "abc"),
   ];
 }
