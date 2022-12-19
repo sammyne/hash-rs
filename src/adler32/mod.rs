064e69b4b221d25b50843c4f53af12e43958b278
sammyne <xiangminli@alumni.sjtu.edu.cn>
2022-12-19 01:18:04 +0000

    doc(adler32): comment APIs

--- src/adler32/mod.rs
+++ src/adler32/mod.rs
@@ -1,20 +1,45 @@
+//! Module adler32 implements the Adler-32 checksum.
+//!
+//! It is defined in [RFC 1950]:
+//! ```
+//! Adler-32 is composed of two sums accumulated per byte: s1 is
+//! the sum of all bytes, s2 is the sum of all s1 values. Both sums
+//! are done modulo 65521. s1 is initialized to 1, s2 to zero. The
+//! Adler-32 checksum is stored as s2*65536 + s1 in most-
+//! significant-byte first (network) order.
+//! ```
+//!
+//! [RFC 1950]: https://rfc-editor.org/rfc/rfc1950.html
+//!
+
 use std::io::Write;
 
 use crate::{Hash, Hash32};
 
+/// The size of an Adler-32 checksum in bytes.
 pub const SIZE: isize = 4;
 
+/// MODULO is the largest prime that is less than 65536.
 const MODULO: u32 = 65521;
+
+/// NMAX is the largest n such that
+/// 255 * n * (n+1) / 2 + (n+1) * (mod-1) <= 2^32-1.
+/// It is mentioned in RFC 1950 (search for "5552").
 const NMAX: usize = 5552;
 
+/// checksum returns the Adler-32 checksum of data.
 pub fn checksum(data: &[u8]) -> u32 {
     update(1, data)
 }
 
+/// new returns a new hash.Hash32 computing the Adler-32 checksum. Its [sum][crate::Hash::sum] method will lay the
+/// value out in big-endian byte order.
 pub fn new() -> impl Hash32 {
     Digest::new()
 }
 
+/// digest represents the partial evaluation of a checksum.
+/// The low 16 bits are s1, the high 16 bits are s2.
 pub struct Digest(u32);
 
 impl Digest {
