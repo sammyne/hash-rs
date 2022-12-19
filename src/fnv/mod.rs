cac6977fb9c67c052099f171548d373f3a206a53
sammyne <xiangminli@alumni.sjtu.edu.cn>
2022-12-19 01:24:19 +0000

    doc(fnv): comment APIs

--- src/fnv/mod.rs
+++ src/fnv/mod.rs
@@ -1,25 +1,44 @@
+//! Module fnv implements FNV-1 and FNV-1a, non-cryptographic hash functions
+//! created by Glenn Fowler, Landon Curt Noll, and Phong Vo.
+//! 
+//! See
+//! <https://en.wikipedia.org/wiki/Fowler-Noll-Vo_hash_function>.
+//!
+
 use crate::{Hash, Hash32, Hash64};
 
+/// new128 returns a new 128-bit FNV-1 [Hash][crate::Hash].
+/// Its [sum][crate::Hash::sum] method will lay the value out in big-endian byte order.
 pub fn new128() -> impl Hash {
     Sum128::new()
 }
 
+/// new128a returns a new 128-bit FNV-1a [Hash][crate::Hash].
+/// Its [sum][crate::Hash::sum] method will lay the value out in big-endian byte order.
 pub fn new128a() -> impl Hash {
     Sum128a::new()
 }
 
+/// new32 returns a new 32-bit FNV-1 [Hash][crate::Hash].
+/// Its [sum][crate::Hash::sum] method will lay the value out in big-endian byte order.
 pub fn new32() -> impl Hash32 {
     Sum32::new()
 }
 
+/// new32a returns a new 32-bit FNV-1a [Hash][crate::Hash].
+/// Its [sum][crate::Hash::sum] method will lay the value out in big-endian byte order.
 pub fn new32a() -> impl Hash32 {
     Sum32a::new()
 }
 
+/// new64 returns a new 64-bit FNV-1 [Hash][crate::Hash].
+/// Its [sum][crate::Hash::sum] method will lay the value out in big-endian byte order.
 pub fn new64() -> impl Hash64 {
     Sum64::new()
 }
 
+/// new64a returns a new 64-bit FNV-1a [Hash][crate::Hash].
+/// Its [sum][crate::Hash::sum] method will lay the value out in big-endian byte order.
 pub fn new64a() -> impl Hash64 {
     Sum64a::new()
 }
