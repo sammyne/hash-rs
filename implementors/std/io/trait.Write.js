514fb2a188103f8100354d649c414574587a2ed9
sammyne <sammyne@users.noreply.github.com>
2022-12-19 03:19:55 +0000

    Deploying to gh-pages from @ sammyne/hash-rs@819c4c4213c3715606bfee7de63e7cd0c497239d 🚀

--- implementors/std/io/trait.Write.js
+++ implementors/std/io/trait.Write.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"hash":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.65.0/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a> for <a class=\"struct\" href=\"hash/adler32/struct.Digest.html\" title=\"struct hash::adler32::Digest\">Digest</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.65.0/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a> for <a class=\"struct\" href=\"hash/maphash/struct.Hash.html\" title=\"struct hash::maphash::Hash\">Hash</a>"]]
+"hash":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.65.0/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a> for <a class=\"struct\" href=\"hash/maphash/struct.Hash.html\" title=\"struct hash::maphash::Hash\">Hash</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
